0a97a45dd2357631af291347077e15f227eee29d
simonetripodi <simone.tripodi@gmail.com>
2011-12-18 16:04:38 +0100

    Wagon: Deploying site to repository

---
+++ apidocs/assets/navtree_data.js
@@ -0,0 +1,10 @@
+var NAVTREE_DATA =
+[ [ "org.nnsoft.commons.bspi", "org/nnsoft/commons/bspi/package-summary.html", [ [ "Description", "org/nnsoft/commons/bspi/package-descr.html", null, "" ], [ "Interfaces", null, [ [ "ServiceFileParserConstants", "org/nnsoft/commons/bspi/ServiceFileParserConstants.html", null, "" ] ]
+, "" ], [ "Classes", null, [ [ "ServiceFileParserTokenManager", "org/nnsoft/commons/bspi/ServiceFileParserTokenManager.html", null, "" ], [ "ServiceLoader", "org/nnsoft/commons/bspi/ServiceLoader.html", null, "" ], [ "SimpleCharStream", "org/nnsoft/commons/bspi/SimpleCharStream.html", null, "" ], [ "Token", "org/nnsoft/commons/bspi/Token.html", null, "" ] ]
+, "" ], [ "Exceptions", null, [ [ "ParseException", "org/nnsoft/commons/bspi/ParseException.html", null, "" ] ]
+, "" ], [ "Errors", null, [ [ "ServiceConfigurationError", "org/nnsoft/commons/bspi/ServiceConfigurationError.html", null, "" ], [ "TokenMgrError", "org/nnsoft/commons/bspi/TokenMgrError.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ apidocs/reference/lists.js
@@ -0,0 +1,12 @@
+var DATA = [
+      { id:0, label:"org.nnsoft.commons.bspi", link:"org/nnsoft/commons/bspi/package-summary.html", type:"package" },
+      { id:1, label:"org.nnsoft.commons.bspi.ParseException", link:"org/nnsoft/commons/bspi/ParseException.html", type:"class" },
+      { id:2, label:"org.nnsoft.commons.bspi.ServiceConfigurationError", link:"org/nnsoft/commons/bspi/ServiceConfigurationError.html", type:"class" },
+      { id:3, label:"org.nnsoft.commons.bspi.ServiceFileParserConstants", link:"org/nnsoft/commons/bspi/ServiceFileParserConstants.html", type:"class" },
+      { id:4, label:"org.nnsoft.commons.bspi.ServiceFileParserTokenManager", link:"org/nnsoft/commons/bspi/ServiceFileParserTokenManager.html", type:"class" },
+      { id:5, label:"org.nnsoft.commons.bspi.ServiceLoader", link:"org/nnsoft/commons/bspi/ServiceLoader.html", type:"class" },
+      { id:6, label:"org.nnsoft.commons.bspi.SimpleCharStream", link:"org/nnsoft/commons/bspi/SimpleCharStream.html", type:"class" },
+      { id:7, label:"org.nnsoft.commons.bspi.Token", link:"org/nnsoft/commons/bspi/Token.html", type:"class" },
+      { id:8, label:"org.nnsoft.commons.bspi.TokenMgrError", link:"org/nnsoft/commons/bspi/TokenMgrError.html", type:"class" }
+
+    ];
